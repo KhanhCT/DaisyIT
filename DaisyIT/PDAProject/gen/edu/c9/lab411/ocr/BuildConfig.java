9fbad67766e6edae7e2f9c8ed6f171de11d4c037
KhanhCT <trongkhanh.hust@gmail.com>
2018-03-04 09:06:37 +0700

    cleanup
    Signed-off-by: KhanhCT <trongkhanh.hust@gmail.com>

--- DaisyIT/PDAProject/gen/edu/c9/lab411/ocr/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package edu.c9.lab411.ocr;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- DaisyIT/phpserver/.svn/text-base/test.php.svn-base
+++
@@ -1,5 +0,0 @@
-<?php
-    require_once __DIR__ .'/db_connect.php'
-    $db = new DB_CONNECT();
-    echo("OK");
-?>
